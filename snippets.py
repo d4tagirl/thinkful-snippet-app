65ac95d84299d40d476bd170ef05cd18225f3a7d
Daniela Vazquez <daniela.vazquez@gmail.com>
2017-06-24 18:50:36 -0300

    Add parser structure

--- snippets.py
+++ snippets.py
@@ -19,8 +19,9 @@ def get(name):
 
     Returns the snippet.
     """
+    
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
+    return "404: Snippet Not Found"
 
 import argparse
 
@@ -36,8 +37,24 @@ def main():
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
+    
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Get a snippet")
+    get_parser.add_argument("name", help="Name of the snippet")
 
     arguments = parser.parse_args()
+    
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
 
 if __name__ == "__main__":
     main()
