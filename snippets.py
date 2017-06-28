523b750a9d3c39380529351d82dd39f50921c101
Daniela Vazquez <daniela.vazquez@gmail.com>
2017-06-28 17:51:46 -0300

    ass catalog and search functionalities

--- snippets.py
+++ snippets.py
@@ -39,7 +39,7 @@ def get(name):
   """Retrieve the snippet with a given name. """
   logging.info("Retrieving snippet {!r}".format(name))
   with connection, connection.cursor() as cursor:
-    cursor.execute("select message from snippets where keyword=%s", (name,))
+    cursor.execute("select * from snippets where keyword=%s", (name,))
     row = cursor.fetchone()
   if not row:
     # No snippet was found with that name.
@@ -47,8 +47,27 @@ def get(name):
     return None
   else:
     logging.debug("Snippet retrieved successfully.")
-    return row[0]
+    return row
     
+
+
+def catalog():
+  logging.info("Retrieving all keywords from snippets")
+  with connection, connection.cursor() as cursor:
+    cursor.execute("select * from snippets order by keyword")
+    all = cursor.fetchall()
+    logging.info("All snippets keywords retrieved")
+  return all
+
+
+
+def search(word):
+  """Retrieve the snippets that contain a word. """
+  logging.info("Retrieving snippets with the word {!r} on the message".format(word))
+  with connection, connection.cursor() as cursor:
+    cursor.execute("select * from snippets where message like %s", ('%' + str(word) + '%',))
+    search_snippets = cursor.fetchall()
+  return search_snippets
 
 
 
@@ -71,6 +90,15 @@ def main():
   logging.debug("Constructing get subparser")
   get_parser = subparsers.add_parser("get", help="Get a snippet")
   get_parser.add_argument("name", help="Name of the snippet")
+  
+  # Subparser for the catalog command
+  logging.debug("Constructing catalog subparser")
+  catalog_parser = subparsers.add_parser("catalog", help="Catalog of snippets available")
+  
+  # Subparser for the search command
+  logging.debug("Constructing search subparser")
+  search_parser = subparsers.add_parser("search", help="Serch for snippets by key word")
+  search_parser.add_argument("word", help="Keyword for the snippet search")
 
   arguments = parser.parse_args()
     
@@ -91,6 +119,34 @@ def main():
       print("Retrieved snippet: {!r}".format(snippet))
     else:
       print("snippet not found!")
+  
+  elif command == "catalog":
+    all_snippets = catalog()
+    print("Retrieved keywords:")
+    for table in all_snippets:
+      print(table)
+      
+  elif command == "search":
+    filtered_snippets = search(**arguments)
+    print("Retrieved keywords:")
+    for table in filtered_snippets:
+      print(table)
 
 if __name__ == "__main__":
   main()
+  
+
+# Extra challenge
+#
+# Hidden snippets
+# Maybe you don't want all your snippets to be visible in the catalog. Create a column hidden boolean not null default
+# false and have the catalog and search functions filter their results with where not hidden. You can then add
+# optional arguments to the put subcommand such as --hide, which will set the hidden flag, and one of --show,
+# --unhide, --no-hide, or --hide=0, which will reset it. (If neither flag is set, leave the hidden flag as it was.)
+
+# Hints and Tips
+# To add optional arguments you will need to learn some new parts of the argparse module. Take a look at the argparse
+# tutorial if you are in need of guidance.
+
+# To add a column to the table you will need to change the table schema. The alter table command will be your
+# friend here - look it up if you are unfamiliar with how it works.
